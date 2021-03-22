9242954dbcefed200dfd170634be9734b3d04c39
Paulo Vaz <psgvaz@gmail.com>
2021-03-22 19:53:03 -0300

    modulo02 concluido

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,23 @@
+import { useMovies } from "../MovieContex"
+import { MovieCard } from "./MovieCard"
+
 export function Content() {
   // Complete aqui
+  const {genres,handleClickButton,selectedGenre, movies} = useMovies()
+  console.log(movies)
+  return (
+    <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+  )
 }
